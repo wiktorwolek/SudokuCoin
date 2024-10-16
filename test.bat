de5be219c3a4d15bb7b0ef73c5f39a8247807685
wiktorwolek <65127553+wiktorwolek@users.noreply.github.com>
2024-10-16 19:10:39 +0200

    removed timeout from test script

--- test.bat
+++ test.bat
@@ -1,5 +1,3 @@
-start cmd /k py .\SudokuCoinNode.py --port 3001  
-timeout 1
-start cmd /k py .\SudokuCoinNode.py --port 3002 --peer 127.0.0.1:3001     
-timeout 1
+start cmd /k py .\SudokuCoinNode.py --port 3001
+start cmd /k py .\SudokuCoinNode.py --port 3002 --peer 127.0.0.1:3001
 start cmd /k py .\SudokuCoinNode.py --port 3003 --peer 127.0.0.1:3001 127.0.0.1:3002
